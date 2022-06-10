5cd9a3e1e81674ebb004946f877e9e5e0772e4f1
Sylvester Chepape <SylvesterC@outex.co.za>
2022-06-10 15:36:04 +0200

    Add project files.

---
+++ ChuckNorrisAPI/ChucknorrisServices.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisAPI
+{
+    public interface IChucknorrisServices
+    {
+        Task<string> Get();
+    }
+    public class ChucknorrisServices : IChucknorrisServices
+    {
+        private HttpClient _httpClient;
+
+        public ChucknorrisServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<string> Get()
+        {
+            string APIURL = $"?";
+            var response = await _httpClient.GetAsync(APIURL);
+            
+            return await response.Content.ReadAsStringAsync();
+
+        }
+    }
+}
---
+++ ChuckNorrisAPI/Controllers/ChuckNorrisController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChuckNorrisController : ControllerBase
+    {
+     
+
+        private readonly ILogger<ChuckNorrisController> _logger;
+        private readonly IChucknorrisServices _chucknorrisServices;
+
+
+        public ChuckNorrisController(ILogger<ChuckNorrisController> logger, IChucknorrisServices chucknorrisServices)
+        {
+            _logger = logger;
+            _chucknorrisServices = chucknorrisServices;
+
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await _chucknorrisServices.Get();
+          
+        }
+    }
+}
